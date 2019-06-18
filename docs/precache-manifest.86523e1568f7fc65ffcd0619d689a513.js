02da558a1d090379a58e9cf0f5715949a449ac32
Ray Ranola <ray.ranola@vmlyr.com>
2019-06-18 11:25:12 +1000

    build: (docs: delpy script, deploy test for github pages)

---
+++ docs/precache-manifest.86523e1568f7fc65ffcd0619d689a513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9505ab1e3330ade060b3d206ab16a003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f9a1f0f871358cd91b3",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "ae341fc80a547fa001bc",
+    "url": "/static/css/main.e3874ad6.chunk.css"
+  },
+  {
+    "revision": "2f9a1f0f871358cd91b3",
+    "url": "/static/js/2.ba600864.chunk.js"
+  },
+  {
+    "revision": "ae341fc80a547fa001bc",
+    "url": "/static/js/main.eb975617.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
