2f88f0016e9452d4e5cca8baf82c6f71e28a3e1e
Ray Ranola <ray.ranola@vmlyr.com>
2019-06-20 10:13:00 +1000

    feature: (test, base: updated publish, fixed app test for router, added tests for em helper)

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -3,5 +3,5 @@ const DEFAULT_BASE_PX = 16;
 export const em = (targetPx: number, basePx: number = DEFAULT_BASE_PX) => {
 	if (targetPx === 0) return 0;
 	if (basePx === 0) basePx = DEFAULT_BASE_PX;
-	return `${targetPx / basePx}em`;
+	return `${parseFloat((targetPx / basePx).toFixed(3))}em`;
 };
